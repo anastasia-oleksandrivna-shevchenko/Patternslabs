1c08b539fdc37189fafeb02432ad23959809cd1d
anastasia-oleksandrivna-shevchenko <shevchenko.anastasiia.o@chnu.edu.ua>
2025-03-01 18:49:11 +0200

    added 2 pattens: Singleton and Prototype

---
+++ creational/src/ConcretePrototype.java
@@ -0,0 +1,10 @@
+public class ConcretePrototype implements Prototype {
+    private String field;
+    public ConcretePrototype(String field){
+        this.field = field;
+    }
+    @Override
+    public Prototype clone(){
+        return new ConcretePrototype(this.field);
+    }
+}
